592bb3e6426f0729978fea739f1469abcdf6d113
DiogoEmanuel535 <emanueldiogo535@gmail.com>
2025-07-10 12:08:02 -0300

    Update Atividade 1 e).c

---
+++ Atividade 1 e).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius;
+
+    printf("Insira a temperatura em Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    celsius = ((fahrenheit - 32) * 5) / 9;
+    printf("A temperatura em Celsius é: %.2f\n", celsius);
+    printf("A temperatura em Fahrenheit é: %.2f\n", fahrenheit);
+
+    return 0;
+}
