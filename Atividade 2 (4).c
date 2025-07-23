0adc0b2e56c1f3851c82dcc12e508aedd8513fa6
DiogoEmanuel535 <emanueldiogo535@gmail.com>
2025-07-23 10:59:27 -0300

    Update Atividade 2 (4).c

--- Atividade 2 (4).c
+++ Atividade 2 (4).c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    float altura, peso_ideal;
+    float altura, pesoideal;
     const float k = 72.7;
     const int B = 58;
 
     printf("Insira sua altura: ");
     scanf("%f", &altura);
 
-    peso_ideal = (k * altura) - B;
-    printf("O peso ideal da Lenda é: %.2f\n", peso_ideal);
+    pesoideal = (k * altura) - B;
+    printf("O peso ideal da Lenda é: %.2f\n", pesoideal);
 
     return 0;
 }
